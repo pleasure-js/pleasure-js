ff696e7df3ab873b4ad0d122765b69ef8f60921c
Martin Rafael Gonzalez <tin@devtin.io>
2020-05-20 11:32:13 -0400

    initial

---
+++ build/guide/SETUP.md
@@ -0,0 +1,16 @@
+- Create project
+- Start API in development mode (edit routes on the fly)
+- Start API in production mode
+- Defining API endpoints
+- Using CRUD operations from entities
+- Flux events
+- Authenticated endpoints / access
+- UI 
+
+Using the command line, I'm gonna create a *pleasure* project:
+
+```bash
+$ pls create my-project
+```
+
+After doing so I should be able to retrieve
---
+++ test/setup.js
@@ -0,0 +1,20 @@
+import test from 'ava'
+
+test.skip(`Creating a project`, t => {
+  /**
+   * Using the command line, we are gonna create a *pleasure* project:
+   */
+
+})
+
+test.only(`Configuration file: pleasure.config.js`, t => {
+  /**
+   * The file `pleasure.config.js` located at the top level directory of the project, holds most of the configuration
+   * needed in order to have a RESTFul API up and running.
+   *
+   * This section is divided
+   */
+
+  cliTest(`pls create`)
+})
+test.todo(`Configuration file`)
